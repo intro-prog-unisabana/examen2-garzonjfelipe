bb9fb488154d64b03fc9ade0f53b97dec51c9fb3
Juan Felipe Garzon Baron <garzonbaronj@gmail.com>
2026-04-14 11:44:26 -0500

    examen 2. 10

--- lap_timer_client.py
+++ lap_timer_client.py
@@ -19,19 +19,7 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-    # TODO: Pedir el nombre del archivo al usuario usando input()
     
-    # TODO: Abrir el archivo y leer el numero de vueltas n
-    
-    # TODO: Crear el cronometro usando lap_timer.init(n)
-    
-    # TODO: Leer los n tiempos de vuelta y agregarlos con lap_timer.add_lap()
-    
-    # TODO: Imprimir la racha decreciente mas larga
-    #       usando lap_timer.longest_decreasing_streak()
     
    
 
